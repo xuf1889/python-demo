e23282e93f534720f8dbf431d142ea7c8d870543
xuf <xuf1889@gmail.com>
2018-05-06 22:31:22 +0800

    add oop module

---
+++ oop/Student.py
@@ -0,0 +1,12 @@
+class Student(object):
+    def __init__(self , name , age):
+        self.__name = name
+        self.__age = age
+
+    def print_info(self):
+        print('name = ' , self.__name , ',age = ' , self.__age)
+
+
+stu = Student('张三' , 16)
+print(stu)
+stu.print_info();
